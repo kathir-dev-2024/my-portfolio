23e024952046139f2f59626f690aef6461f4d623
Kathir-2024 <kathiresankathir947@gmail.com>
2024-12-11 18:14:39 +0530

    added a new project in project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
-import todoappimg from "../assets/Todo-app.png"
-import websiteimg2 from "../assets/food-ecommerce.jpg";
-import blogimg from "../assets/BS-blog-page.png"
+import todoappimg from "../assets/Todo-app.png";
+import socialappimg from "../assets/Social-app.png";
+import blogimg from "../assets/BS-blog-page.png";
 
 export default function Projects() {
   const config = {
@@ -12,10 +12,10 @@ export default function Projects() {
         link: "https://react-todolistapp-project.netlify.app/",
       },
       {
-        image: websiteimg2,
+        image: socialappimg,
         description:
-          "Food E-commerce website similar to Swiggy, built with Angular & .NET.",
-        link: "https://example-foodecommerce.com",
+          "Social Media App for posting, editing, and deleting posts, built with React & localStorage.",
+        link: "https://social-post-app-react.netlify.app/",
       },
       {
         image: blogimg,
@@ -31,7 +31,6 @@ export default function Projects() {
       id="projects"
       className="flex flex-col py-20 bg-primary px-5 justify-center text-white"
     >
-
       <div className="w-full text-center mb-10">
         <h1 className="text-4xl font-bold border-b-4 border-secondary inline-block mb-5">
           Projects
@@ -42,20 +41,18 @@ export default function Projects() {
         </p>
       </div>
 
-      
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-5">
         {config.projects.map((project, index) => (
           <div
             key={index}
             className="relative group overflow-hidden rounded-lg shadow-lg bg-gray-800 hover:scale-105 transform transition-transform duration-300"
           >
-           
             <img
               className="h-60 w-full object-cover"
               src={project.image}
               alt={`Project ${index + 1}`}
             />
-            
+
             <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center">
               <p className="text-center px-4 text-sm md:text-base">
                 {project.description}
